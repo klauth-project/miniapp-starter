29f86f2377412adbda550e9249029a0856421a46
kayrules <kayrules@gmail.com>
2024-07-02 22:29:23 +0800

    updated package.json

--- cli.sh
+++ cli.sh
@@ -28,15 +28,16 @@ grep -rl __PROJECT__ . | xargs sed -i "" -e 's/__PROJECT__/'$_project'/g'
 grep -rl __MODULE__ . | xargs sed -i "" -e 's/__MODULE__/'$_module'/g'
 grep -rl __PORT__ . | xargs sed -i "" -e 's/__PORT__/'$_port'/g'
 
-cd package
 
 echo '--- Installing dependencies ---'
+cd package
 yarn add react react-dom react-router-dom react-router ionicons @ionic/react @ionic/react-router
 
 echo '--- Installing dev dependencies ---'
 yarn add -D @babel/plugin-transform-runtime @babel/preset-env @babel/preset-react babel-loader css-loader html-webpack-plugin prop-types style-loader webpack webpack-cli webpack-dev-server webpack-merge
 
 echo '--- Finalizing ---'
+cd ..
 mv package $_pwd/$_module
 rm -rf .temp
 
